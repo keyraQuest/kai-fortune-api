4b2a5230c064eaaafa2ab6cb494406f020270e44
keyraQuest <key.ra.creates@gmail.com>
2025-04-05 21:14:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import random
 
 app = Flask(__name__)
 
-@app.route("/fortune")
-def get_fortune():
-    with open("kai_fortunes.txt", "r") as f:
+@app.route('/')
+def home():
+    return "Kai Fortune API is live. Use /fortune to get a random fortune."
+
+@app.route('/fortune')
+def fortune():
+    with open('kai_fortunes.txt', 'r') as f:
         fortunes = f.readlines()
-    fortune = random.choice(fortunes).strip()
-    return fortune
+    return random.choice(fortunes)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
